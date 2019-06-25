e6fec9dafd5fa5166cf3e3eaabdfbbe913ab6b95
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-25 20:24:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,16 @@ end
 end
 
 def age_appropriate_birthday(birthday_kids)
+<<<<<<< HEAD
     birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age < 13
       puts "You are too old for this." if age >= 13
     end
+=======
+  birthday_kids.each do |kids_name, age|
+  if age < 13  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end
+end
+>>>>>>> f5944102d4b777ee3338b52e9b3f9033ea0a6435
 end
